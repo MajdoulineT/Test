232704188498b321356b8a2d0c30fce28826a2d6
MajdoulineT <tounassi_majdouline@hotmail.com>
2025-09-10 20:48:06 +0200

    MAJ

--- script.php
+++ script.php
@@ -1,15 +1,33 @@
 <?php 
 
+$temperature= 39;
+
+switch ($temperature) {
+    case 37:
+    echo("tout va bien");
+    break;
+    case 38:
+    echo("légèrement fiévreux");
+    break;
+    case 39:
+    echo("vraiment fiévreux");
+    break;
+    case 40:
+    echo("très fiévreux");
+    break;
+}
+?>
+
+<?php 
 $temperature= 30;
 
 if ($temperature > 40) {
     echo"le bain est trop chaud";
 
-} elseif ($temperature < 25) { 
-    echo"Le bain est trop froid";
+} elseif ($temperature < 25) {
+    echo"le bain est trop froid";
 
-} else { 
-    echo"Le bain est à la bonne température";
-
+} else {
+    echo"le bain est à la bonne température";
 }
 ?>
