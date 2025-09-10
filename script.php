1eceaa695ffdf488fc1907a14305632c3ad78ef2
MajdoulineT <tounassi_majdouline@hotmail.com>
2025-09-10 18:12:16 +0200

    New repository

---
+++ script.php
@@ -0,0 +1,15 @@
+<?php 
+
+$temperature= 30;
+
+if ($temperature > 40) {
+    echo"le bain est trop chaud";
+
+} elseif ($temperature < 25) { 
+    echo"Le bain est trop froid";
+
+} else { 
+    echo"Le bain est à la bonne température";
+
+}
+?>
